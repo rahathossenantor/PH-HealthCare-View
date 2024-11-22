87b96f7ea63a938ef920d5608a5177fb15fa589d
Md Rahat Hossen Antor <md.rahathossenantor@gmail.com>
2024-11-22 19:45:36 +0600

    Login page created

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,7 +3,7 @@ import assets from "@/assets";
 import Image from "next/image";
 import Link from "next/link";
 
-const Register = () => {
+const Login = () => {
     return (
         <Container sx={{ py: 5 }}>
             <Stack sx={{
@@ -26,31 +26,25 @@ const Register = () => {
                             <Image src={assets.svgs.logo} width={50} alt="logo" />
                         </Box>
                         <Box>
-                            <Typography variant="h5" fontWeight={700}>Patient Register</Typography>
+                            <Typography variant="h5" fontWeight={700}>Login</Typography>
                         </Box>
                     </Stack>
                     <form>
                         <Grid container spacing={2} sx={{ my: 1 }}>
-                            <Grid item md={12}>
-                                <TextField label="Full Name" variant="outlined" size="small" type="text" fullWidth />
-                            </Grid>
                             <Grid item md={6}>
                                 <TextField label="Email" variant="outlined" size="small" type="email" fullWidth />
                             </Grid>
                             <Grid item md={6}>
                                 <TextField label="Password" variant="outlined" size="small" type="password" fullWidth />
                             </Grid>
-                            <Grid item md={6}>
-                                <TextField label="Contact Number" variant="outlined" size="small" type="tel" fullWidth />
-                            </Grid>
-                            <Grid item md={6}>
-                                <TextField label="Address" variant="outlined" size="small" type="text" fullWidth />
-                            </Grid>
                             <Grid item md={12}>
-                                <Button variant="contained" fullWidth>Register</Button>
+                                <Button variant="contained" fullWidth>Login</Button>
                             </Grid>
                         </Grid>
-                        <Typography component="p" sx={{ mt: 2 }}>Already have an account? <Link href="/login" className="text-blue-600">Login</Link> </Typography>
+                        <Stack direction="row" justifyContent="space-between">
+                            <Typography component="p" sx={{ mt: 2 }}>Forgot password? <Link href="" className="text-blue-600">Reset Here</Link> </Typography>
+                            <Typography component="p" sx={{ mt: 2 }}>Don&apos;t have an account? <Link href="/register" className="text-blue-600">Register</Link> </Typography>
+                        </Stack>
                     </form>
                 </Box>
             </Stack>
@@ -58,4 +52,4 @@ const Register = () => {
     );
 };
 
-export default Register;
+export default Login;
