9d3d280415f69b5d22076ab1a386692d5fda05ae
Md Rahat Hossen Antor <md.rahathossenantor@gmail.com>
2024-12-14 19:44:36 +0600

    private route implemented on dashboard

--- src/app/(dashboardLayout)/layout.tsx
+++ src/app/(dashboardLayout)/layout.tsx
@@ -1,6 +1,16 @@
+"use client";
+
 import Dashboard from "@/components/dashboard/Dashboard";
+import { isLoggedIn } from "@/services/auth.services";
+import { useRouter } from "next/navigation";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+    const router = useRouter();
+
+    if (!isLoggedIn()) {
+        router.push("/login");
+    };
+
     return (
         <Dashboard>{children}</Dashboard>
     );
